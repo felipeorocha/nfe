d80fb270485507bf200a7317c421d046036f96ce
Felipe Rocha <felipeo.rocha@hotmail.com>
2019-11-18 22:59:11 -0300

    Fetching API images and rendering list images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchField from './SearchField';
+import ImagesList from './ImagesList';
 
 class App extends Component {
+  state = {
+    imagesList: []
+  };
 
-  onSearchTerm(event, term) {
+  onSearchTerm = async (event, term) => {
     event.preventDefault();
-    console.log(term);
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID dca040ec4115a13484560df329adad673bf8df094a77688818ffaa4894a7bd03'
+      }
+    });
+
+    console.log(response.data.results);
+    this.setState({ imagesList: response.data.results });
+    // .then(response => {
+    //   console.log(response.data.results)
+    // }); // promises way
   }
 
   render() {
     return (
       <div className="ui container">
         <SearchField onSubmit={this.onSearchTerm} />
+        <ImagesList imagesList={this.state.imagesList} />
       </div>
     );
   }
